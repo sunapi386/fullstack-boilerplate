0d0b70a818eaa7d24eb62403b7fe6db5ddaaf9d9
Jason Sun <jason@sunapi386.ca>
2020-04-19 10:52:36 -0700

    Menu icon replaced with dropdown icon

--- packages/web/src/components/NavBar.tsx
+++ packages/web/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react"
+import React from "react"
 import { Link } from "./Link"
 import {
   Avatar,
@@ -16,14 +16,13 @@ import {
   useColorMode,
 } from "@chakra-ui/core/dist"
 import { useMe } from "./providers/MeProvider"
-import { RouteComponentProps } from "@reach/router"
 import { useLogout } from "../lib/hooks/useLogout"
-import { MdMenu } from "react-icons/md"
 
 import { useLocalStorage } from "@noquarter/hooks"
 import {
   FaHome,
   FiCalendar,
+  IoIosArrowDropdown,
   IoIosLogOut,
   WiMoonAltWaningCrescent2,
 } from "react-icons/all"
@@ -75,17 +74,11 @@ const RightNav = () => {
     )
   }
 
-  return (
-    <Flex justifyContent={"flex-end"} m={1}>
-      <Link to="/" p="2">
-        <Box as={FaHome} size="32px" />
-      </Link>
-      <Link to="/hosting" p="2">
-        <Box as={FiCalendar} size="32px" />
-      </Link>
+  const DropDownMenu = () => {
+    return (
       <Menu>
         <MenuButton p="2">
-          <Box as={MdMenu} size="32px" />
+          <Box as={IoIosArrowDropdown} size="32px" />
         </MenuButton>
         <MenuList>
           <MenuItem>
@@ -105,11 +98,23 @@ const RightNav = () => {
           </MenuItem>
         </MenuList>
       </Menu>
+    )
+  }
+
+  return (
+    <Flex justifyContent={"flex-end"} m={1}>
+      <Link to="/" p="2">
+        <Box as={FaHome} size="32px" />
+      </Link>
+      <Link to="/hosting" p="2">
+        <Box as={FiCalendar} size="32px" />
+      </Link>
+      <DropDownMenu />
     </Flex>
   )
 }
 
-export const NavBar: FC<RouteComponentProps> = () => {
+export const NavBar = () => {
   return (
     <Grid templateColumns="repeat(2, 1fr)" gap={6} m="1">
       <LeftTitle />
