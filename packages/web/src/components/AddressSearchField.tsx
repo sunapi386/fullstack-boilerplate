94640e5b571c77ae9181c303a97390aa0de443ad
Jason Sun <jason@sunapi386.ca>
2020-04-23 10:52:39 -0700

    scss wrapper around rsuitejs sheets

--- packages/web/src/components/AddressSearchField.tsx
+++ packages/web/src/components/AddressSearchField.tsx
@@ -1,30 +1,37 @@
 import React from "react"
-import { useColorMode } from "@chakra-ui/core/dist"
+import { Box, useColorMode } from "@chakra-ui/core/dist"
 
 // rsuite stuff
 import { AutoComplete } from "rsuite"
 
 // according to https://github.com/rsuite/rsuite#usage
 // 1. I should import the styles
-import 'rsuite/lib/styles/index.less'; // or 'rsuite/dist/styles/rsuite-default.css'
+//import 'rsuite/lib/styles/index.less'; // or 'rsuite/dist/styles/rsuite-default.css'
+import "./AddressSearchField.scss"
 
 // 2. tried this as it seems more specific to AutoComplete, seems to have no no effect either
-import "rsuite/lib/AutoComplete/styles"
+// import "rsuite/lib/AutoComplete"
 
 // 3. resorting to try this, but it overwrites all the styles from chakra-ui
-import 'rsuite/dist/styles/rsuite-default.css'
+// import 'rsuite/dist/styles/rsuite-default.css'
 
 // 4. also I needed to do dark theme too
 interface AddressSearchFieldProps {
   data: string[]
 }
 
-export const AddressSearchField = ({ data, ...props }: AddressSearchFieldProps) => {
-  const {colorMode} = useColorMode() // colorMode === "light" ? lightTheme : darkTheme
-  return <AutoComplete
-    // not needed it seems
-    // style={}
-    data={data}
-    {...props}
-  />
+export const AddressSearchField = ({
+  data,
+  ...props
+}: AddressSearchFieldProps) => {
+  const { colorMode } = useColorMode() // colorMode === "light" ? lightTheme : darkTheme
+  return (
+    <AutoComplete
+      classPrefix={colorMode === "light" ? "rsuite-light" : "rsuite-dark"}
+      className={colorMode === "light" ? "rsuite-light" : "rsuite-dark"}
+      // not needed it seems
+      data={data}
+      {...props}
+    ></AutoComplete>
+  )
 }
