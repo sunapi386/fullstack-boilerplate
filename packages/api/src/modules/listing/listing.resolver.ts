5a01d6e7e6946d5abea3ed79fd704f80c082a0d5
Jason Sun <jason@sunapi386.ca>
2020-04-24 18:56:10 -0700

    add listing author fieldresolver

--- packages/api/src/modules/listing/listing.resolver.ts
+++ packages/api/src/modules/listing/listing.resolver.ts
@@ -1,4 +1,13 @@
-import { Arg, Authorized, Mutation, Query, Resolver } from "type-graphql"
+import {
+  Arg,
+  Authorized,
+  FieldResolver,
+  Mutation,
+  Query,
+  Resolver,
+  ResolverInterface,
+  Root,
+} from "type-graphql"
 import { Inject } from "typedi"
 
 import { Listing } from "./listing.entity"
@@ -7,11 +16,14 @@ import { CurrentUser } from "../shared/context/currentUser"
 import { User } from "../user/user.entity"
 import { CreateListingInput } from "./inputs/createlisting.input"
 import { UserInputError } from "apollo-server-express"
+import { UserRepository } from "../user/user.repository"
 
 @Resolver(() => Listing)
-export class ListingResolver {
+export class ListingResolver implements ResolverInterface<Listing> {
   @Inject(() => ListingService)
   listingService: ListingService
+  @Inject(() => UserRepository)
+  private userRepository: UserRepository
 
   // anyone can read listings collection
   @Query(() => [Listing])
@@ -43,5 +55,15 @@ export class ListingResolver {
   @Mutation(() => Boolean)
   async deleteListingNumber(@Arg("id") id: string): Promise<boolean> {
     return this.listingService.destroy(id)
+  }
+
+  @FieldResolver()
+  async author(@Root() listing: Listing): Promise<User> {
+    const user = await this.userRepository.findById(listing.authorId, {
+      cache: 1000,
+    })
+    if (!user)
+      throw new UserInputError("Cannot find authorId " + listing.authorId)
+    return user
   }
 }
