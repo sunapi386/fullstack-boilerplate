e7c65b170e748f1d4a82ea2198791c1078b4a65b
Jason Sun <jason@sunapi386.ca>
2020-04-25 23:31:23 -0700

    temp config on cors

--- packages/web/src/lib/config.tsx
+++ packages/web/src/lib/config.tsx
@@ -1,20 +1,28 @@
 let env: string
 
+// hostname: what the browser tells me my page's URL location is
+export const myDomains = [
+  "https://sunpi.co",
+  "http://10.0.1.21",
+  "http://73.158.125.228",
+]
+
 const hostname = window?.location?.hostname
-if (hostname === "www.sunpi.co") {
+if (myDomains.includes(hostname)) {
   env = "production"
 } else {
   env = "development"
 }
+// I am on production if I am where I should be
 
 export const environment = env
 
 export const production = env === "production"
 
 export const apiUrl = production
-  ? "https://api.sunpi.co/graphql"
-  : "http://localhost:5555/graphql"
+  ? "http://10.0.1.21:5555/graphql"
+  : "https://fancystack.herokuapp.com/graphql"
 
-export const webUrl = production
-  ? "https://www.sunpi.co"
-  : "http://localhost:3000"
+export const myDomainDefault = "https://sunpi.co"
+
+export const webUrl = production ? myDomainDefault : "http://10.0.1.21:3000"
