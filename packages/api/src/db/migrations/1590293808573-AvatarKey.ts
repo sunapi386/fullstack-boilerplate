5abe1a5bbeef841a16a01d4e91bfb92d5564a9d5
Jason Sun <jason@sunapi386.ca>
2020-05-25 13:32:31 -0700

    upload to different paths; alter table avatarKey only

--- packages/api/src/db/migrations/1590293808573-AvatarKey.ts
+++ packages/api/src/db/migrations/1590293808573-AvatarKey.ts
@@ -4,18 +4,10 @@ export class AvatarKey1590293808573 implements MigrationInterface {
   name = "AvatarKey1590293808573"
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "avatarUrl"`)
-    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "avatar"`)
-    await queryRunner.query(
-      `ALTER TABLE "user" ADD "avatarKey" character varying`,
-    )
+    await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "avatarUrl" TO "avatarKey"`)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "avatarKey"`)
-    await queryRunner.query(`ALTER TABLE "user" ADD "avatar" character varying`)
-    await queryRunner.query(
-      `ALTER TABLE "user" ADD "avatarUrl" character varying`,
-    )
+    await queryRunner.query(`ALTER TABLE "user" RENAME COLUMN "avatarKey" TO "avatarUrl"`)
   }
 }
